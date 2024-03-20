5747e062feecc431fe0b48d276c7e1ed60f91874
Dev Jobalia <webdev.jobalia@gmail.com>
2024-03-20 18:43:08 +0530

    HOME -> TASK COMPONENT

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import "package:firebase_core/firebase_core.dart";
 import 'package:flutter/material.dart';
 import 'package:untitled/auth/main_page.dart';
 import "package:untitled/firebase_options.dart";
+import 'package:untitled/widgets/task-widgets.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Main_Page(),
+      home: Task_Widget(),
     );
   }
 }
