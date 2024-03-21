9dbb5210371097abeb47d5c1a14631c6ba4b9a14
Dev Jobalia <webdev.jobalia@gmail.com>
2024-03-22 00:43:43 +0530

    AUTH USER STORE ON FIRESTORE 🗃️

--- lib/data/auth_data.dart
+++ lib/data/auth_data.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:untitled/data/firestore.dart';
 // import 'package:untitled/data/firestore.dart';
 
 abstract class AuthenticationDatasource {
@@ -21,7 +22,7 @@ class AuthenticationRemote extends AuthenticationDatasource {
           .createUserWithEmailAndPassword(
               email: email.trim(), password: password.trim())
           .then((value) {
-        // Firestore_Datasource().CreateUser(email);
+        Firestore_Datasource().CreateUser(email);
       });
     }
   }
